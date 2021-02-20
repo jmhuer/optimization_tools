874451109548373e407a3fed03f62750614650fd
Juan M Huerta <jmhuer@gmail.com>
2021-02-20 01:37:50 -0500

    changing gdrive to id

--- utils/gdrive_download.py
+++ utils/gdrive_download.py
@@ -2,8 +2,8 @@ import re
 import subprocess
 
 
-def download_gdrive(share_link, local_name, print_stout=False):
-  id = re.search('d/(.*?)/view', share_link).group(1)
+def download_gdrive(id, local_name, print_stout=False):
+  id = re.search('/(.*?)/', id).group(1)
   command = "wget --load-cookies /tmp/cookies.txt \"https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate \'https://docs.google.com/uc?export=download&id={}\' -O- | sed -rn \'s/.*confirm=([0-9A-Za-z_]+).*/\\1\\n/p')&id={}\"  -O \'{}\' && rm -rf /tmp/cookies.txt".format(id,id,local_name)
   returned_value = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   if print_stout: print(returned_value.stdout.decode("utf-8"))
