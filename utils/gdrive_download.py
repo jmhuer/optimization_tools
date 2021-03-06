04b25434cdde74187c44a69c5984deaa021b0c7b
Juan M Huerta <jmhuer@gmail.com>
2021-03-05 18:44:58 -0600

    changed download_gdrive

--- utils/gdrive_download.py
+++ utils/gdrive_download.py
@@ -2,10 +2,9 @@ import re
 import subprocess
 
 
-def download_gdrive(id, local_name, print_stout=False):
-  id = re.search('/(.*?)/', id).group(1)
-  command = "wget --load-cookies /tmp/cookies.txt \"https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate \'https://docs.google.com/uc?export=download&id={}\' -O- | sed -rn \'s/.*confirm=([0-9A-Za-z_]+).*/\\1\\n/p')&id={}\"  -O \'{}\' && rm -rf /tmp/cookies.txt".format(id,id,local_name)
-  returned_value = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+def download_gdrive(id, local_name, print_stout=True):
+  coomand = 'gdown https://drive.google.com/uc?id={}'.format(id)
+  returned_value = subprocess.run(coomand, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   if print_stout: print(returned_value.stdout.decode("utf-8"))
-  else: print("Download Complete: " + local_name)
+  else: print("Download Complete")
 
