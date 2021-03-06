50c83c1aae614d64720e39e1efc3cae27f76dcde
Juan M Huerta <jmhuer@gmail.com>
2021-03-05 18:47:51 -0600

    .

--- utils/gdrive_download.py
+++ utils/gdrive_download.py
@@ -2,7 +2,7 @@ import re
 import subprocess
 
 
-def download_gdrive(id, local_name, print_stout=True):
+def download_gdrive(id, print_stout=True):
   coomand = 'gdown https://drive.google.com/uc?id={}'.format(id)
   returned_value = subprocess.run(coomand, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
   if print_stout: print(returned_value.stdout.decode("utf-8"))
